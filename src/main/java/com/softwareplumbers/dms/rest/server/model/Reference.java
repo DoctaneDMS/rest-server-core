a33eebf304993fa471cb8464fc3c64dfaeae1e88
jonathan.essex <jonathan.essex@misys.com>
2018-06-13 22:37:59 +0200

    Javadoc

--- src/main/java/com/softwareplumbers/dms/rest/server/model/Reference.java
+++ src/main/java/com/softwareplumbers/dms/rest/server/model/Reference.java
@@ -7,29 +7,39 @@ import javax.json.JsonValue;
 /** Class representing a reference to a specific version of a document in a repository.
  * 
  * @author Jonathan Essex
- *
  */
 public class Reference implements Comparable<Reference> {
+	
+	/** Document identifier */
 	public final String id;
+	/** Version number */
 	public final Integer version;
+	/** Construct a reference to a particular version of a document */
 	public Reference(String id, Integer version) { this.id = id; this.version = version; }
+	/** Construct a reference to the latest version of a document */
 	public Reference(String id) { this.id = id; this.version = null; }
 	
+	/** Get the identifier */
 	public String getId() { return id; }
+	/** Get the version number */
 	public Integer getVersion() { return version; }
+	/** Compare two references for equality */
+	public boolean equals(Reference other) { return compareTo(other) == 0; }
 	
-	public boolean equals(Reference other) { 
-		return compareTo(other) == 0;
-	}
-	
+	/** Compare a reference for equality with another object */	
 	public boolean equals(Object other) {
 		return other instanceof Reference && equals((Reference)other);
 	}
 	
+	/** Create a hash value for this reference */		
 	public int hashCode() {
 		return id.hashCode() ^ (version == null ? 0 : version.intValue());
 	}
 
+	/** Compare two references
+	 *
+	 * A reference (id, null) is greater than a reference (id, version). 
+	 */		
 	public int compareTo(Reference other) {
 		int result = id.compareTo(other.id);
 		if (result != 0) return result;
@@ -41,6 +51,13 @@ public class Reference implements Comparable<Reference> {
 		return 0;
 	}
 	
+	/** Create a reference from a JSON object.
+	 * 
+	 * The JSON representation of a reference is { id: _id_, version _version_ }
+	 * 
+	 * @param object
+	 * @return A reference object
+	 */
 	public static Reference fromJSON(JsonObject object) {
 		JsonNumber version = object.getJsonNumber("version");
 		return new Reference(
@@ -48,7 +65,13 @@ public class Reference implements Comparable<Reference> {
 				version != null && version != JsonValue.NULL ? (Integer)version.intValue() : null);
 	}
 	
+	/** Create a string from a reference.
+	 * 
+	 * The string representation of a reference is id:version
+	 * 
+	 * @return id:version
+	 */
 	public String toString() {
-		return id + (version == null ? "" : "." + version);
+		return id + (version == null ? "" : ":" + version);
 	}
 }
