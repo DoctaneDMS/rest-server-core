957a9e33a16cefb532b8ff4dcbdc9b858fe9a671
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2019-02-23 13:52:36 -0500

    Oh ffs why didn't eclipse commit this???

---
+++ src/main/java/com/softwareplumbers/dms/rest/server/model/Constants.java
@@ -0,0 +1,9 @@
+package com.softwareplumbers.dms.rest.server.model;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+public class Constants {
+	public static final String ROOT_ID = null; // Root workspace Id
+	public static final JsonObject EMPTY_METADATA = Json.createObjectBuilder().build();
+}
