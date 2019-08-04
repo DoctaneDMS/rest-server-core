366d0e2941b8c3e99fa21e651aef1c9baadc62bd
Jonathan Essex <je@fincloud.io>
2019-08-04 10:00:37 -0400

    SAML2 configuration improvement - allow issued id to be specified
    More fixes under #60 
    
    seems to work now but defer closing issue until tested on public site.

--- src/main/java/com/softwareplumbers/dms/rest/server/core/SAMLSignonService.java
+++ src/main/java/com/softwareplumbers/dms/rest/server/core/SAMLSignonService.java
@@ -8,8 +8,10 @@ package com.softwareplumbers.dms.rest.server.core;
 import com.softwareplumbers.dms.rest.server.model.SAMLProtocolHandlerService;
 import com.softwareplumbers.dms.rest.server.model.SAMLProtocolHandlerService.SAMLOutputError;
 import com.softwareplumbers.dms.rest.server.model.SignonService;
+import java.util.Optional;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+import org.springframework.beans.factory.annotation.Required;
 
 
 /** Redirects caller to a sign-on service using a simple URI template.
@@ -18,21 +20,43 @@ import javax.ws.rs.core.UriBuilder;
  */
 public class SAMLSignonService implements SignonService {
     
-    public SAMLProtocolHandlerService samlService;
-    public String assertionConsumerURI;
+    SAMLProtocolHandlerService samlService;
+    String assertionConsumerURI;
+    Optional<String> issuerId;
             
-    public SAMLSignonService(SAMLProtocolHandlerService samlService, String assertionConsumerURI) {
+    public SAMLSignonService(SAMLProtocolHandlerService samlService, String assertionConsumerURI, Optional<String> issuerId) {
         this.samlService = samlService;
         this.assertionConsumerURI = assertionConsumerURI;
+        this.issuerId = issuerId;
+    }
+    
+    public SAMLSignonService() {
+        samlService = null;
+        assertionConsumerURI = null;
+        issuerId = Optional.empty();
+    }
+    
+    @Required
+    public void setSAMLProtocolHandlerService(SAMLProtocolHandlerService samlService) {
+        this.samlService = samlService;
     }
 
+    @Required
+    public void setAssertionConsumerURI(String assertionConsumerURI) {
+        this.assertionConsumerURI = assertionConsumerURI;
+    }
+    
+    public void setIssuerId(String issuerId) {
+        this.issuerId = Optional.of(issuerId);
+    }
+    
     @Override
     public Response redirect(String relayState) {
         
         try {
             UriBuilder builder = UriBuilder
                 .fromUri(samlService.getIDPEndpoint())
-                .queryParam("SamlRequest", samlService.formatRequest(assertionConsumerURI))
+                .queryParam("SAMLRequest", samlService.formatRequest(assertionConsumerURI, issuerId))
                 .queryParam("RelayState", relayState);
             return Response.seeOther(builder.build()).build();
         } catch (SAMLOutputError err) {
