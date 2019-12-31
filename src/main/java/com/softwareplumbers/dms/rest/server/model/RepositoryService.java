80db38ebb5257cd2bfea7bd1c73c5c2832d99450
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2019-12-31 12:00:30 -0500

    moved some methods around - per #68

--- src/main/java/com/softwareplumbers/dms/rest/server/model/RepositoryService.java
+++ src/main/java/com/softwareplumbers/dms/rest/server/model/RepositoryService.java
@@ -26,6 +26,8 @@ import javax.ws.rs.core.MediaType;
  */
 public interface RepositoryService {
     
+    //-------------- Exception classes --------------//
+    
     public static class BaseException extends Exception {
         public BaseException(String description) { super(description); }
     }
@@ -101,7 +103,9 @@ public interface RepositoryService {
 			
 		}
 	}
-	
+
+    //-------------- Simple Document API ------------//
+    
 	/** Get a document from a Reference.
  	 * 
 	 * @param reference
@@ -109,7 +113,49 @@ public interface RepositoryService {
 	 * @throws InvalidReference if there is no document matching the reference in the repository 
 	 */
 	public Document getDocument(Reference reference) throws InvalidReference;
-
+    
+	/** Create a new document in the repository
+	 * 
+	 * @param mediaType the type of document
+	 * @param stream a supplier function that produces a stream of binary data representing the document
+	 * @param metadata a Json object describing the document
+	 * @return A Reference object that can later be used to retrieve the document
+	 * @throws InvalidWorkspace if workspace does not exist (and createWorkspace is false)
+	 * @throws InvalidWorkspaceState if workspace is already closed
+	 */
+	public Reference createDocument(MediaType mediaType, 
+			InputStreamSupplier stream, 
+			JsonObject metadata);
+    
+	/** Update a document in the repository.
+	 * <p>
+	 * Update a document in the repository. Uses an id rather than a Reference because a Reference
+	 * also encompasses the version of the document. This function creates a new version of the document
+	 * rather than actually updating the old document.
+	 * </p><p>
+	 * If any parameter is null, this implies the value will be unchanged by the update operation. Thus,
+	 * it is possible to update a document's meta-data without actually updating the document itself
+	 * by simply setting the stream parameter to null.
+	 * </p><p>
+	 * Setting the workspace in this function does not remove the document from any workspace it is already
+	 * in; it merely adds it to the specified workspace.
+	 * </p><p>
+	 * Meta-data passed in to the update operation will be merged with the existing metadata; for example,
+	 * if a property is missing in the supplied meta-data object, that property will remain unchanged. To
+	 * actually remove meta-data it is necessary to explicitly pass in a JsonObject.NULL value.
+	 * </p>
+	 * @param id A string id for the document.
+	 * @param mediaType (optional) new media type for document
+	 * @param stream (optional) a supplier function that produces a stream of binary data representing the document 
+	 * @param metadata (optional) a json object describing the document
+	 * @return A reference to the new version of the document.
+	 * @throws InvalidDocumentId if no document exists with the given id
+	 */
+	public Reference updateDocument(String id, 
+		MediaType mediaType, 
+		InputStreamSupplier stream, 
+		JsonObject metadata) throws InvalidDocumentId;
+    
 	/** Get a document from an Id and a workspace Id.
 	 * 
 	 * Gets the most recent version of a document in the given workspace.
@@ -124,19 +170,8 @@ public interface RepositoryService {
 	 */
 	public DocumentLink getDocumentLink(String workspaceId, QualifiedName path, String documentId) throws InvalidWorkspace, InvalidObjectName, InvalidDocumentId;
     
-	/** Create a new document in the repository
-	 * 
-	 * @param mediaType the type of document
-	 * @param stream a supplier function that produces a stream of binary data representing the document
-	 * @param metadata a Json object describing the document
-	 * @return A Reference object that can later be used to retrieve the document
-	 * @throws InvalidWorkspace if workspace does not exist (and createWorkspace is false)
-	 * @throws InvalidWorkspaceState if workspace is already closed
-	 */
-	public Reference createDocument(MediaType mediaType, 
-			InputStreamSupplier stream, 
-			JsonObject metadata);
-	
+	// ---------------- Workspace API -------------- //
+    
 	/** Create a new document in the repository.
 	 * 
 	 * Creates document within the given workspace. The first parts of the given name are the
@@ -252,35 +287,6 @@ public interface RepositoryService {
 			JsonObject metadata, 
 			boolean createWorkspace,
 			boolean createDocument) throws InvalidWorkspace, InvalidObjectName, InvalidWorkspaceState;
-	
-	/** Update a document in the repository.
-	 * <p>
-	 * Update a document in the repository. Uses an id rather than a Reference because a Reference
-	 * also encompasses the version of the document. This function creates a new version of the document
-	 * rather than actually updating the old document.
-	 * </p><p>
-	 * If any parameter is null, this implies the value will be unchanged by the update operation. Thus,
-	 * it is possible to update a document's meta-data without actually updating the document itself
-	 * by simply setting the stream parameter to null.
-	 * </p><p>
-	 * Setting the workspace in this function does not remove the document from any workspace it is already
-	 * in; it merely adds it to the specified workspace.
-	 * </p><p>
-	 * Meta-data passed in to the update operation will be merged with the existing metadata; for example,
-	 * if a property is missing in the supplied meta-data object, that property will remain unchanged. To
-	 * actually remove meta-data it is necessary to explicitly pass in a JsonObject.NULL value.
-	 * </p>
-	 * @param id A string id for the document.
-	 * @param mediaType (optional) new media type for document
-	 * @param stream (optional) a supplier function that produces a stream of binary data representing the document 
-	 * @param metadata (optional) a json object describing the document
-	 * @return A reference to the new version of the document.
-	 * @throws InvalidDocumentId if no document exists with the given id
-	 */
-	public Reference updateDocument(String id, 
-		MediaType mediaType, 
-		InputStreamSupplier stream, 
-		JsonObject metadata) throws InvalidDocumentId;
 	
     /** Update or create a link to an existing document by reference.
      * 
