6d0aca57e0a2ffae86ddac17b68c0a9a6ed06604
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2020-01-08 11:43:46 -0500

    Interface classes moved to dms-interface-core

--- src/main/java/com/softwareplumbers/dms/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.softwareplumbers.dms;
-
-import javax.json.Json;
-import javax.json.JsonObject;
-
-public class Constants {
-	public static final String ROOT_ID = null; // Root workspace Id
-	public static final JsonObject EMPTY_METADATA = Json.createObjectBuilder().build();
-}
