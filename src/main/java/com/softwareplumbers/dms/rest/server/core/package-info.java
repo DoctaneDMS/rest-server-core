eb71734bf7f0f4045226319b9cbe29dabd3438e4
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2020-01-14 18:08:27 -0500

    javadoc

---
+++ src/main/java/com/softwareplumbers/dms/rest/server/core/package-info.java
@@ -0,0 +1,7 @@
+/** A RESTful API exposing a Doctane RepositoryService.
+ * 
+ * This package implements a RESTful web service (using Spring Boot and JAX-RS) 
+ * which exposes a Doctane RepositoryService via a straightforward REST API.
+ * 
+ */
+package com.softwareplumbers.dms.rest.server.core;
